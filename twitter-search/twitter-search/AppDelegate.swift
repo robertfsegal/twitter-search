84acf8e92500c4f8b8818b90c1bd70d9376be5fe
Robert Segal <rob@getsetgames.com>
2016-03-06 00:20:02 -0500

    initial osx toolbar application setup

---
+++ twitter-search/twitter-search/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  twitter-search
+//
+//  Created by Robert Segal on 2016-03-05.
+//  Copyright © 2016 Get Set Games Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
