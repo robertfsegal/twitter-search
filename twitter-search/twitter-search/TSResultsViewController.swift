55f277eba48db983f638d39cc066e2fb9326f980
Robert Segal <rob@getsetgames.com>
2016-03-12 00:37:36 -0500

    View controller for the popover

---
+++ twitter-search/twitter-search/TSResultsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TSResultsViewController.swift
+//  twitter-search
+//
+//  Created by Robert Segal on 2016-03-06.
+//  Copyright © 2016 Get Set Games Inc. All rights reserved.
+//
+
+import Cocoa
+
+class TSResultsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
