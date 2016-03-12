63de4260a8cb9ab71cd9a7241f21d0b507a2dacc
Robert Segal <rob@getsetgames.com>
2016-03-12 00:46:28 -0500

    Show the popover when user clicks on the toolbar icon

--- twitter-search/twitter-search/AppDelegate.swift
+++ twitter-search/twitter-search/AppDelegate.swift
@@ -13,15 +13,40 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
+    let popover = NSPopover()
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = Selector("togglePopover:")
+        }
+        
+        popover.contentViewController = TSResultsViewController(nibName: "TSResultsViewController", bundle: nil)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
+    func showPopover(sender: AnyObject?) {
 
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MinY)
+        }
+    }
+    
+    func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    
+    func togglePopover(sender: AnyObject?) {
+        if popover.shown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
+        }
+    }
 }
 
