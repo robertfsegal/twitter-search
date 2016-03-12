7b496803bef7172ab88f67872fe549800ae1e463
Robert Segal <rob@getsetgames.com>
2016-03-12 17:22:34 -0500

    [change] pull in tweets when user changes what's typed [change] add in web policy delegate handler to filter links clicked and defer to external browser

--- twitter-search/twitter-search/TSResultsViewController.swift
+++ twitter-search/twitter-search/TSResultsViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import Cocoa
+import WebKit
 
-class TSResultsViewController: NSViewController {
+class TSResultsViewController: NSViewController, NSSearchFieldDelegate, WebPolicyDelegate {
 
+    @IBOutlet weak var search: NSSearchField!
+    @IBOutlet weak var web: WebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
+    override func controlTextDidChange(obj: NSNotification)
+    {
+        let url = NSURL(string: "http://localhost:3000/tweets?s=" + search.stringValue)
+        let req = NSURLRequest(URL: url!)
+        
+        web.mainFrame.loadRequest(req)
+    }
+    
+    func webView(webView: WebView!, decidePolicyForNavigationAction
+       actionInformation: [NSObject : AnyObject]!,
+                 request: NSURLRequest!,
+                   frame: WebFrame!,
+        decisionListener listener: WebPolicyDecisionListener! )
+    {
+        let s = request.URL?.absoluteString
+        
+        if (s!.containsString("localhost") == false)
+        {
+            // open the url in an external application
+            //
+            NSWorkspace.sharedWorkspace().openURL(request.URL!)
+            listener.ignore()
+        }
+        else
+        {
+            listener.use()
+        }
+    }
+ 
 }
