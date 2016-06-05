4ea5e9bbb96225bc39645ef68dc8ca739c746c06
Robert Segal <rob@getsetgames.com>
2016-06-05 00:12:17 -0400

    OSX platform initial commit

---
+++ platforms/osx/TwitterSearch/TSResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TSResultsViewController.h
+//  TwitterSearch
+//
+//  Created by Robert Segal on 2016-06-04.
+//  Copyright © 2016 __CDV_ORGANIZATION_NAME__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TSResultsViewController : NSViewController
+
+@end
---
+++ platforms/osx/TwitterSearch/TSResultsViewController.m
@@ -0,0 +1,22 @@
+//
+//  TSResultsViewController.m
+//  TwitterSearch
+//
+//  Created by Robert Segal on 2016-06-04.
+//  Copyright © 2016 __CDV_ORGANIZATION_NAME__. All rights reserved.
+//
+
+#import "TSResultsViewController.h"
+
+@interface TSResultsViewController ()
+
+@end
+
+@implementation TSResultsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
